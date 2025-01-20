60679be6baf3a8063060739daee86e3af4845d52
WikanSolution <wikansolution@gmail.com>
2025-01-20 22:44:51 +0800

    tambah autofree

--- WKLabel.pas
+++ WKLabel.pas
@@ -11,11 +11,16 @@ type
     FLicenseFeature: string;
     FTagObject: TObject;
     FPrimaryKey: String;
+    destructor Destroy; override;
     { Private declarations }
   protected
     { Protected declarations }
   public
+    AutoFreeTaggedObject: Boolean;
     constructor Create(AOwner: TComponent); override;
+
+    //pisAutoFree akan otomatis free object yang di tag di constructor
+    procedure setTagObject(pObject: TObject; pisAutoFree: Boolean);
     { Public declarations }
   published
     property LicenseFeature: string read FLicenseFeature write FLicenseFeature;
@@ -38,8 +43,24 @@ begin
   inherited Create(AOwner);
    AutoSize := True;
    Tag := 9;
+   AutoFreeTaggedObject := False;
  // WordWrap := True;
   // TODO -cMM: TWKLabel.Create default body inserted
+end;
+
+destructor TWKLabel.Destroy;
+begin
+  inherited Destroy;
+  if AutoFreeTaggedObject then begin
+      if FTagObject <> nil then FreeAndNil(FTagObject);
+
+  end;
+end;
+
+procedure TWKLabel.setTagObject(pObject: TObject; pisAutoFree: Boolean);
+begin
+   AutoFreeTaggedObject := pisAutoFree;
+   TagObject := pObject;
 end;
 
 end.
